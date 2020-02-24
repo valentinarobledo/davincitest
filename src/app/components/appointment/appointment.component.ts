a193223d943f0fdd783a2c9dbdb15bc4045717c6
valentinarobledo <valenrobledo31@gmail.com>
2020-02-24 00:52:02 -0500

    Davinci Test in Angular

--- src/app/components/appointment/appointment.component.ts
+++ src/app/components/appointment/appointment.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 
+
 @Component({
   selector: 'app-appointment',
   templateUrl: './appointment.component.html',
   styleUrls: ['./appointment.component.css']
 })
 export class AppointmentComponent implements OnInit {
+	model:any = {}
+	
+	add():void{
+
+	}
 
   constructor() { }
 
---
+++ src/app/models/appointment.ts
@@ -0,0 +1,13 @@
+export class Appointment {
+	name: string;
+	lastname: string;
+	identification: string;
+	pais: string;
+	dpto: string;
+	city: string;
+	address: string;
+	phone: string;
+	email: string;
+	time: string;
+	date: string;
+}
